b5907f2206f139371f225871c0c75220377f8afb
Viet Tran <vietnamtran@gmail.com>
2020-07-20 21:19:28 +0000

    Task 0 tested code, updated header

--- binary_trees.h
+++ binary_trees.h
@@ -33,8 +33,6 @@ typedef struct binary_tree_s heap_t;
 
 /* FUNCTION PROTOTYPES */
 /* provided print function prototypes */
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 /* function to insert new node (0) */
