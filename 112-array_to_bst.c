8e339d297eaf871deaeaeef7bb2a048f050a7456
Kelsie Merchant <eislek02@gmail.com>
2020-07-24 12:06:03 -0500

    Update code to ignore duplicate values instead of fail

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -19,7 +19,28 @@ bst_t *array_to_bst(int *array, size_t size)
 	{
 		node = bst_insert(&tree, array[i]);
 		if (node == NULL)
-			return (NULL);
+		{
+			if (bst_search(tree, array[i]) == NULL)
+			    return (NULL);
+		}
 	}
 	return (tree);
+}
+
+/**
+ * bst_search - finds location of where the value should be inserted
+ * @tree: tree to sort in binary search order
+ * @value: value to find where should be inserted
+ *
+ * Return: pointer to location to insert value at
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+	if (tree->n == value)
+		return ((bst_t *)tree);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	return (bst_search(tree->right, value));
 }
