bce7d9b214f0df1bfbebb8dd975053eafa0c823b
Kelsie Merchant <eislek02@gmail.com>
2020-07-24 15:43:03 -0500

    Add function to remove node from binary search tree and update header file to include function prototype; tested locally

--- binary_trees.h
+++ binary_trees.h
@@ -115,5 +115,9 @@ bst_t *location_finder(bst_t *tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 /* function that searches for a value in a Binary Search Tree (27)*/
 bst_t *bst_search(const bst_t *tree, int value);
+/* function to remove node from bst, replace with inorder successor (28)*/
+bst_t *bst_remove(bst_t *root, int value);
+/* helper function to find node to remove */
+bst_t *bst_search2(const bst_t *tree, int value);
 
 #endif
